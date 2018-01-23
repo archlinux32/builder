25c3af1f1af84f79a44155078c1cbc1705c723b2
Erich Eckner <git@eckner.net>
2018-01-23 09:37:31 +0100

    web-scripts/packages.php: use same user as other scripts

--- web-scripts/packages.php
+++ web-scripts/packages.php
@@ -1,20 +1,20 @@
 <?php
 
-$mysql = new mysqli("localhost", "webserver", "empty");
-if ($mysql->connect_error) {
-  die("Connection failed: " . $mysql->connect_error);
-}
-
-$result = $mysql -> query("SELECT * FROM buildmaster.binary_packages");
-if ($result -> num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-    foreach ($row as $key => $val) {
-      print $key .": ".$val." - ";
-    }
-    print "<br>\n";
+  $mysql = new mysqli("localhost", "http", "http");
+  if ($mysql->connect_error) {
+    die("Connection to mysql database failed: " . $mysql->connect_error);
   }
-}
 
-print 'OK';
+  $result = $mysql -> query("SELECT * FROM buildmaster.binary_packages");
+  if ($result -> num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      foreach ($row as $key => $val) {
+        print $key .": ".$val." - ";
+      }
+      print "<br>\n";
+    }
+  }
+
+  print 'OK';
 
 ?>
