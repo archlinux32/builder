fd0d0003abae69107063b63ea26d6ce3feac9ecb
Erich Eckner <git@eckner.net>
2018-01-29 23:08:33 +0100

    web-scripts/broken-packages.php: fix link

--- web-scripts/broken-packages.php
+++ web-scripts/broken-packages.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" type="text/css" href="/static/style.css">
 </head>
 <body>
-<a href="build-logs/">build logs</a><br>
+<a href="/build-logs/">build logs</a><br>
 <?php
 
 $mysql = new mysqli("localhost", "webserver", "empty", "buildmaster");
