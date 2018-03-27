181901d9ae18fc4024972769321a26b88a646ed2
Erich Eckner <git@eckner.net>
2018-03-27 11:12:20 +0200

    web-scripts/build-slaves.php: display count of logged lines

--- web-scripts/build-slaves.php
+++ web-scripts/build-slaves.php
@@ -6,24 +6,26 @@
     die("Connection to mysql database failed: " . $conn->connect_error);
   }
 
-  $result =
+  if (!$result =
     $conn->query(
       "SELECT" .
       " `build_slaves`.`name`," .
       "`build_slaves`.`operator`," .
       "`package_sources`.`pkgbase`," .
-      "`build_slaves`.`last_connection`" .
+      "`build_slaves`.`last_connection`," .
+      "`build_slaves`.`logged_lines`" .
       " FROM `build_slaves`" .
       " LEFT JOIN `build_assignments` ON" .
       " `build_slaves`.`currently_building`=`build_assignments`.`id`" .
       " LEFT JOIN `package_sources` ON" .
       " `build_assignments`.`package_source`=`package_sources`.`id`" .
       " ORDER BY `build_slaves`.`last_connection`"
-    );
+    ))
+    die($conn->error);
 
   print "<table border=1>\n";
   if ($result->num_rows > 0) {
-    print "<tr><th>name</th><th>operator</th><th>currently building</th><th>last connection</th></tr>\n";
+    print "<tr><th>name</th><th>operator</th><th>currently building</th><th>last connection</th><th>logged lines</th></tr>\n";
     while ($row = $result -> fetch_assoc()) {
       foreach ($row as $key => $value) {
         if ($value=="") {
@@ -35,6 +37,7 @@
       print "<td>".$row["operator"]."</td>";
       print "<td>".$row["pkgbase"]."</td>";
       print "<td>".$row["last_connection"]."</td>";
+      print "<td>".$row["logged_lines"]."</td>";
       print "</tr>\n";
     }
   }
